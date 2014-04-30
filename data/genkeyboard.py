4fc43c99b6a438eca45c487afad1ced32edfe84d
Nick <nickljeffrey@gmail.com>
2014-04-29 23:08:31 -0500

    putting the A key in every position

--- data/genkeyboard.py
+++ data/genkeyboard.py
@@ -2,9 +2,12 @@ from PIL import Image
 
 keyb = Image.open("img/BLANK.png")
 pix = keyb.load()
-pix[0, 0] = (255, 0, 0, 255)
 
 img=Image.open('img/a.png','r')
-keyb.paste(img,(3,3))
+(width, height) = keyb.size
+for y in range(height):
+    for x in range(width):
+        if pix[x, y] == (255, 0, 0, 255):
+            keyb.paste(img, (x, y))
 
 keyb.save("img/custom.png")
