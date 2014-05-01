a3526297c54e546ca0dab5a78b7a0e099e374688
Nick <nickljeffrey@gmail.com>
2014-04-30 19:52:11 -0500

    saving keyboard into img/custom.png

--- data/genkeyboard.py
+++ data/genkeyboard.py
@@ -1,13 +1,24 @@
 from PIL import Image
+import json
 
 keyb = Image.open("img/BLANK.png")
 pix = keyb.load()
-
-img=Image.open('img/a.png','r')
+keys = json.loads(open("layout.data", 'r').read())
 (width, height) = keyb.size
+index = 0
 for y in range(height):
     for x in range(width):
         if pix[x, y] == (255, 0, 0, 255):
-            keyb.paste(img, (x, y))
+            key = keys[index]
+            if key == "\\":
+                key = "bslash"
+            elif key == "/":
+                key = "fslash"
+            elif key == ".":
+                key = "dot"
+            elif key == ";":
+                key = "semi"
+            keyb.paste(Image.open('img/'+key+'.png','r'), (x, y))
+            index += 1
 
-keyb.save("img/custom.png")
+keyb.save("../img/custom.png")
